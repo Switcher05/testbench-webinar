1439b1c3d361d4da458e270c03d7d9002d70c5b7
Johannes Häyry <johannes.hayry@vaadin.com>
2016-03-17 14:49:19 +0200

    Set viewport size for PhantomJS

--- src/test/java/my/vaadin/crm/DriverUtil.java
+++ src/test/java/my/vaadin/crm/DriverUtil.java
@@ -1,5 +1,6 @@
 package my.vaadin.crm;
 
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
@@ -9,7 +10,10 @@ public class DriverUtil {
 		try {
 			String headless = System.getProperty("phantomjs");
 			if (Boolean.TRUE.equals(Boolean.valueOf(headless))) {
-				return new PhantomJSDriver();
+				PhantomJSDriver driver = new PhantomJSDriver();
+				driver.manage().window().setSize(new Dimension(1280, 720));
+				return driver;
+
 			}
 		} catch (SecurityException e) {
 			e.printStackTrace();
